e402c45e041182b309bdf13baee4a44839e29e64
ebarrera2019263 <ebarrera2019263>
2024-05-28 20:00:21 -0600

    se cargo

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -15,7 +15,6 @@ const PortfolioPage = () => {
     return (
         <ContainerPage>
             <TransitionPage />
-            <AvatarPortfolio />
             <CircleImage />
             <div className="flex flex-col justify-center h-full">
                 <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">Mis últimos <span className="font-bold text-secondary">trabajos realizados</span></h1>
